f3316a6425304efd016f6bcc5724968c6462d0cd
arielcami <71854140+arielcami@users.noreply.github.com>
2023-08-17 19:29:04 -0500

    CazadoresRecuperanFlechas.lua
    Re-escritura del script desde cero.
    Mejora de eficiencia, funcionalidad y legibilidad.

--- FlechaCazadores.lua
+++ FlechaCazadores.lua
@@ -1,108 +1,56 @@
-local function KILL(E,P,N)    
-	if N:IsWorldBoss() then return end 		--> No aplica en Bosses
+local SCRIPT_ON = 1
+local ANNOUNCE 	= 1
 
-	local C, R, L = P:GetClassMask(), math.random(1,4), P:GetLevel()
-	local EL, f_e, alg, resta = math.random(3,9), "|cffff9efcRecuperaste más flechas por muerte élite.","|cffff9efcRecuperaste algunas flechas."
-	local resta, elite = (P:GetLevel() - N:GetLevel()), N:IsElite()
+local arrows = {2512,2515,3030,3031,3464,9399,10579,11285,19316,18042,12654,28053,24417,
+	30611,31949,24412,28056,33803,34581,32760,31737,41586,30319,41165,52021}
 
-	if resta <= -10 then return end 		--> Si el mob te lleva 10 niveles o más, el script termina.
+local allowedCreatureTypes = {1, 2, 3, 4, 5, 6, 7, 9}
+-- 1=Bestia  2=Dragonante  3=Demonio  4=Elemental  5=Gigante  6=No-Muerto  7=Humanoide  9=Mecánico
 
-	if C==4 then  							--> Clase 4, Cazador.
-		if resta <= 4 then
-			if P:IsHonorOrXPTarget(N) then
-
-		if P:HasItem(2512) and L <= 9 then if elite then P:AddItem(2512,EL) P:SendBroadcastMessage(f_e,0)
-		else P:AddItem(2512,R) P:SendBroadcastMessage(alg,0) end
-
-		elseif P:HasItem(2515) and L >=10 and L <=24 then 
-		if elite then P:AddItem(2515,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(2515,R) P:SendBroadcastMessage(alg,0) end
-
-		elseif P:HasItem(3030) and L >=25 and L <=39 then 
-		if elite then P:AddItem(3030,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(3030,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(3464) and L >=25 and L <=39  then 
-		if elite then P:AddItem(3464,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(3464,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(9399) and L >=35 and L <=39 then 
-		if elite then P:AddItem(9399,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(9399,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(11285) and L >=40 and L <=54 then 
-		if elite then P:AddItem(11285,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(11285,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(19316) and L >=51 and L <=59 then 
-		if elite then P:AddItem(19316,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(19316,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(18042) and L >=52 and L <=59 then 
-		if elite then P:AddItem(18042,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(18042,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(12654) and L ==54 then 
-		if elite then P:AddItem(12654,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(12654,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(28053) and L >=55 and L <=64 then 
-		if elite then P:AddItem(28053,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(28053,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(24417) and L >=61 and L <=68 then 
-		if elite then P:AddItem(24417,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(24417,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(28056) and L >=65 and L <=74 then 
-		if elite then P:AddItem(28056,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(28056,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(30611) and L >=66 and L <=74 then 
-		if elite then P:AddItem(30611,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(30611,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(31949) and L >=68 and L <=76 then 
-		if elite then P:AddItem(31949,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(31949,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(34581) and L >=70 and L <=79 then 
-		if elite then P:AddItem(34581,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(34581,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(31737) and L >=70 and L <=79 then 
-		if elite then P:AddItem(31737,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(31737,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(30319) and L >=70 and L <=80 then 
-		if elite then P:AddItem(30319,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(30319,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(41165) and L >=71 and L <=80 then 
-		if elite then P:AddItem(41165,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(41165,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(41586) and L >=75 and L <=80 then 
-		if elite then P:AddItem(41586,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(41586,R) P:SendBroadcastMessage(alg,0) end
-
-	elseif P:HasItem(52021)  and L >=80 then
-		if elite then P:AddItem(52021,EL) P:SendBroadcastMessage(f_e,0)
-			else P:AddItem(52021,R) P:SendBroadcastMessage(alg,0) end
+	local function IS_ALLOWED_CREATURE_TYPE(NPC)
+		local bool = false
+		for i=1, #allowedCreatureTypes do
+			if allowedCreatureTypes[i] == NPC:GetCreatureType() then 
+				bool = true
+			end		
 		end
-	else
-		 P:SendBroadcastMessage('No puedes recuperar flechas de un objetivo que no te aporta XP.')	
+		return bool
 	end
-	else 
-		P:SendBroadcastMessage('No puedes recuperar flechas de un objetivo con ese nivel.')	
-		end
-	end 
+
+local function On_PvE_Kill (Event, Player, Npc)
+
+	if SCRIPT_ON==1  then
+
+		if Npc:IsWorldBoss() then return end 	--> No aplica en Bosses
+									
+		local LEVEL_DIFF = math.abs(Player:GetLevel() - Npc:GetLevel())
+
+		if LEVEL_DIFF >= 5 then return end 	--> No te recompensa si el nivel del NPC es demasiado alto o demasiado bajo.
+
+		if Player:GetClassMask() == 4 then --> Si es Cazador.
+
+			local ELITE = Npc:IsElite()	
+
+			if IS_ALLOWED_CREATURE_TYPE(Npc) then	--> Accede solo si la criatura es de tipo permitido.
+				for i=1, #arrows do
+					if Player:HasItem(arrows[i]) then 
+						if ELITE then
+							Player:AddItem(arrows[i], math.random(2,6))
+						else
+							Player:AddItem(arrows[i], math.random(1,3))
+						end
+						break
+					end
+				end
+			end
+		end	
+	end
 end
------------------------------
+RegisterPlayerEvent(7, On_PvE_Kill)
+
 local function OnLogin (E,P)
-	if P:GetClassMask() == 4 then
-	P:SendBroadcastMessage('|cff36ff00Cazadores|r pueden recuperar flechas de sus víctimas, pero no balas.') else return end
+	if (P:GetClassMask()==4) and (SCRIPT_ON==1) and (ANNOUNCE==1) then
+		P:SendBroadcastMessage('Los |cff36ff00Cazadores|r recuperan flechas de sus víctimas.') 
+	end
 end
-----------------------------
-RegisterPlayerEvent(7, KILL)
 RegisterPlayerEvent(3, OnLogin)
